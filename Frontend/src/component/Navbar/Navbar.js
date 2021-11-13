f4738eccfc6edab2d547e347547d67b16c4ec8a0
kenamehta <kenamehta1995@gmail.com>
2021-11-13 00:09:04 -0800

    User Profile mongoose

--- Frontend/src/component/Navbar/Navbar.js
+++ Frontend/src/component/Navbar/Navbar.js
@@ -1,47 +1,63 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import { auth } from "../../service/auth";
+import axios from "axios";
+import { base } from "../../config/address";
 
-class NavigationBar extends Component {
-    handleLogout = () => {
-        auth.signOut();
-        localStorage.clear();
-    };
-    render() {
-        if (localStorage.getItem("user")) {
-            let user = JSON.parse(localStorage.getItem("user"));
-            let message = "Hi, " + user.displayName.split(" ")[0];
-            let photoURL = user.photoURL;
-            return (
-                <Navbar expand="lg" bg="dark" variant="dark">
-                    <Navbar.Brand href="/home">Finding Ohana</Navbar.Brand>
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto"></Nav>
-                        <Nav>
-                            <Nav.Link href="/profile"><img className="navbar-photoURL" alt="" src={photoURL ? photoURL : "default_photoURL.jpg"}></img></Nav.Link>
-                            <NavDropdown title={message}>
-                                <NavDropdown.Item href={"/profile/" + user.uid}>View Profile</NavDropdown.Item>
-                                <NavDropdown.Item href="/login" onClick={this.handleLogout}>Logout</NavDropdown.Item>
-                            </NavDropdown>
-                            <Nav.Link  ></Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-            );
-        }
-
-        else
-            return (
-                <Navbar expand="lg" bg="dark" variant="dark">
-                    <Navbar.Brand href="/home">Finding Ohana</Navbar.Brand>
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto"></Nav>
-                        <Nav>
-                            <Nav.Link href="/login">Sign-Up/Login</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-            );
+export default function NavigationBar() {
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      const user = JSON.parse(localStorage.getItem("user"));
+      const userID = user.uid;
+      const userName = user.displayName;
+      const payload = { userID, userName };
+      axios.post(base + "/profile", payload);
     }
+  });
+  const handleLogout = () => {
+    auth.signOut();
+    localStorage.clear();
+  };
+  if (localStorage.getItem("user")) {
+    let user = JSON.parse(localStorage.getItem("user"));
+    let message = "Hi, " + user.displayName.split(" ")[0];
+    let photoURL = user.photoURL;
+    return (
+      <Navbar expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="/home">Finding Ohana</Navbar.Brand>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto"></Nav>
+          <Nav>
+            <Nav.Link href="/profile">
+              <img
+                className="navbar-photoURL"
+                alt=""
+                src={photoURL ? photoURL : "default_photoURL.jpg"}
+              ></img>
+            </Nav.Link>
+            <NavDropdown title={message}>
+              <NavDropdown.Item href={"/profile/" + user.uid}>
+                View Profile
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/login" onClick={handleLogout}>
+                Logout
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  } else
+    return (
+      <Navbar expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="/home">Finding Ohana</Navbar.Brand>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto"></Nav>
+          <Nav>
+            <Nav.Link href="/login">Sign-Up/Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
 }
-export default NavigationBar;
--- Frontend/src/component/ProfilePage/ProfilePage.js
+++ Frontend/src/component/ProfilePage/ProfilePage.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { base } from "../../config/address";
+import axios from "axios";
 
 export default function ProfilePage(props) {
-
-    console.log(props.match.params.userID)
-    return <div/>
+  const userID = props.match.params.userID;
+  useEffect(() => {
+    axios.get(base + `/profile/${userID}`);
+  }, [userID]);
+  console.log(props.match.params.userID);
+  return <div />;
 }
