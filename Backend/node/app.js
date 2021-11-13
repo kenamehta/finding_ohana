77cfc9260e4ec07876e9e3268d7f1f489cd66f0d
kenamehta <kenamehta1995@gmail.com>
2021-11-12 18:07:11 -0800

    remove unnecessary lines

--- Backend/node/app.js
+++ Backend/node/app.js
@@ -1,7 +1,5 @@
 var express = require("express");
-
 var app = express();
-// var cors = require("cors");
 const Mongoose = require("mongoose");
 
 Mongoose.connect(
@@ -17,7 +15,6 @@ Mongoose.connect(
     console.log(err);
   });
 
-//  app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -41,20 +38,5 @@ app.use(function(req, res, next) {
   next();
 });
 app.use("/profile", require("./routes/profile"));
-// app.use("/student/job_list", require("./routes/Student/studentJobs"));
-// app.use("/student/event_list", require("./routes/Student/studentEvents"));
-// app.use("/student/profile", require("./routes/Student/studentProfile"));
-// app.use(
-//   "/student/applications",
-//   require("./routes/Student/studentApplications")
-// );
-// app.use("/student/all_students", require("./routes/Student/studentAll"));
-// app.use("/company/profile", require("./routes/Company/companyProfile"));
-// app.use("/company/job/students", require("./routes/Company/companyJobStudent"));
-// app.use("/company/event_list", require("./routes/Company/companyEvents"));
-// app.use(
-//   "/company/event/students",
-//   require("./routes/Company/companyEventStudent")
-// );
 
 module.exports = app;
