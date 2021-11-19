7fcbcd1e4c8a7cda96909a5a5418eab889e69d1e
architdhingra <archit@nickelytics.com>
2021-11-19 00:08:10 -0800

    added send and accept request api

--- Backend/ProjectCode/MongoDb.py
+++ Backend/ProjectCode/MongoDb.py
@@ -5,5 +5,5 @@ from pymongo import MongoClient
 
 def getConnection():
     client = MongoClient('mongodb+srv://archit:qazwsxedc@cluster0.ocdka.mongodb.net/Cluster0?retryWrites=true&w=majority', ssl_cert_reqs=ssl.CERT_NONE)
-    mydb = client["cmpe295db"]
+    mydb = client["Cluster0"]
     return mydb
