95a0b9e445c0afaef88e2c1bca43d24e6b1bd271
kenamehta <kenamehta1995@gmail.com>
2021-11-18 19:57:37 -0800

    addding userId to post schema

--- Backend/node/model/post.js
+++ Backend/node/model/post.js
@@ -3,6 +3,7 @@ const Mongoose = require("mongoose");
 export const postSchema = new Mongoose.Schema({
   _id: String,
   content: String,
+  userId: String,
 });
 
 module.exports = { Post: Mongoose.model("Post", postSchema) };
