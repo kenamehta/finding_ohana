888298a1f7e67c375722b093489584ba1c2ca86d
kenamehta <kenamehta1995@gmail.com>
2021-11-20 15:43:52 -0800

    recommended post setup

--- Frontend/src/component/LandingPage/LandingPage.js
+++ Frontend/src/component/LandingPage/LandingPage.js
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Card, Button, Container, Form } from "react-bootstrap";
-import CardMedia from "@material-ui/core/CardMedia";
+import {
+  Card,
+  Button,
+  Container,
+  Form,
+  Row,
+  Col,
+  Image,
+} from "react-bootstrap";
 import "./style.css";
 import axios from "axios";
 import { base } from "../../config/address";
@@ -9,9 +16,6 @@ import { base } from "../../config/address";
 const useStyles = makeStyles({
   root: {
     maxWidth: 800,
-  },
-  media: {
-    height: 140,
   },
 });
 const authUser = JSON.parse(localStorage.getItem("user"));
@@ -27,14 +31,17 @@ if (authUser) {
 
 export default function MediaCard() {
   const [newPostContent, setNewPostContent] = useState("");
+  const [recommendedPosts, setRecommendedPosts] = useState([]);
   const classes = useStyles();
+
   const onShare = (e) => {
     e.preventDefault();
     axios
       .post(`${base}/post/${authUser.uid}`, { newPostContent })
       .then((response) => {
         if (response.status === 200) {
-          const recommendedPosts = response.data.payload;
+          const recommendedPosts = response.data.payload.recommendedPosts;
+          setRecommendedPosts(recommendedPosts);
           setNewPostContent("");
         }
       });
@@ -43,6 +50,7 @@ export default function MediaCard() {
   const onChangeNewPostContent = (e) => {
     setNewPostContent(e.target.value);
   };
+
   return (
     <Container className={classes.root}>
       <Card border="light" className="mt-4 card-style">
@@ -71,22 +79,51 @@ export default function MediaCard() {
       </Card>
 
       <h3 className="mt-4 large-block">Posts you might be interested in...</h3>
+      {recommendedPosts.map((post) => null)}
       <Card className="card-style mt-3">
-        <div className="d-flex">
-          <div className="col-3">
-            <CardMedia
-              className={classes.media}
-              image={"https://randomuser.me/api/portraits/men/32.jpg"}
+        <Row className="py-3 px-4">
+          <Col md={2}>
+            <Image
+              className="post-photo"
+              src="https://randomuser.me/api/portraits/men/32.jpg"
+              roundedcircle="true"
             />
-          </div>
-          <div className="col-7">
-            {/* <CardHeader
-              title="Volleball Game today!!!"
-              subheader="We are a group of 5 looking for more people to join us on weekend volleball games."
-              className="comment-name"
-            /> */}
-          </div>
-        </div>
+          </Col>
+          <Col md={10}>
+            <Row className="large-block">John Doe</Row>
+            <Row className="small-block">Volleyball at SJSU court anyone?</Row>
+          </Col>
+        </Row>
+      </Card>
+      <Card className="card-style mt-3">
+        <Row className="py-3 px-4">
+          <Col md={2}>
+            <Image
+              className="post-photo"
+              src="https://randomuser.me/api/portraits/men/32.jpg"
+              roundedcircle="true"
+            />
+          </Col>
+          <Col md={10}>
+            <Row className="large-block">John Doe</Row>
+            <Row className="small-block">Volleyball at SJSU court anyone?</Row>
+          </Col>
+        </Row>
+      </Card>
+      <Card className="card-style mt-3">
+        <Row className="py-3 px-4">
+          <Col md={2}>
+            <Image
+              className="post-photo"
+              src="https://randomuser.me/api/portraits/men/32.jpg"
+              roundedcircle="true"
+            />
+          </Col>
+          <Col md={10}>
+            <Row className="large-block">John Doe</Row>
+            <Row className="small-block">Volleyball at SJSU court anyone?</Row>
+          </Col>
+        </Row>
       </Card>
     </Container>
   );
