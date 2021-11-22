05b79448810f45310c18f7a4df2d18ccb65f639b
kenamehta <kenamehta1995@gmail.com>
2021-11-22 00:53:37 -0800

    format

--- Frontend/src/config/auth-methods.js
+++
@@ -1,6 +0,0 @@
-import firebase from "./firebase-config";
-export const authMethods = [
-  firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-  firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-  firebase.auth.EmailAuthProvider.PROVIDER_ID,
-];
