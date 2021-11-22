6a8d93d573e323a26dd3c67f342e95f0f5b4c628
kenamehta <kenamehta1995@gmail.com>
2021-11-22 00:51:19 -0800

    format

--- Frontend/src/config/firebase-config.js
+++ Frontend/src/config/firebase-config.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCe0f40DywIW0K07qFE9Y8i3OuIkDHoe94",
-    authDomain: "findingohana-6ac58.firebaseapp.com",
-    projectId: "findingohana-6ac58",
-    storageBucket: "findingohana-6ac58.appspot.com",
-    messagingSenderId: "392784623225",
-    appId: "1:392784623225:web:53bfce5092a69336ee86a8",
-    measurementId: "G-DJM91HPBMT"
+  apiKey: "AIzaSyCe0f40DywIW0K07qFE9Y8i3OuIkDHoe94",
+  authDomain: "findingohana-6ac58.firebaseapp.com",
+  projectId: "findingohana-6ac58",
+  storageBucket: "findingohana-6ac58.appspot.com",
+  messagingSenderId: "392784623225",
+  appId: "1:392784623225:web:53bfce5092a69336ee86a8",
+  measurementId: "G-DJM91HPBMT",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
